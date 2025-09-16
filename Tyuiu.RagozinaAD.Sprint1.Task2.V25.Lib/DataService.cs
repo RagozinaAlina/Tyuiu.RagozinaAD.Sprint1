f8f7da3f615f0fae05b1c67abecaed7f673d6fdc
Алина Рагозина <Tyuiu.RagozinaAD@outlook.com>
2025-09-16 20:07:44 +0500

    Task2

--- Tyuiu.RagozinaAD.Sprint1.Task2.V25.Lib/DataService.cs
+++ Tyuiu.RagozinaAD.Sprint1.Task2.V25.Lib/DataService.cs
@@ -5,7 +5,7 @@ namespace Tyuiu.RagozinaAD.Sprint1.Task2.V25.Lib
     {
         public double ConvertRadsToDegrees(int value)
         {
-            double degrees = value * (180 / 3.14);
+            double degrees = value * (180 / Math.PI);
             return Math.Round(degrees, 3);
         }
     }
