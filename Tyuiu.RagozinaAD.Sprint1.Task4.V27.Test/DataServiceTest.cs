48c58aa71d3b451f70c03ebb2ccef72e68736e09
Алина Рагозина <Tyuiu.RagozinaAD@outlook.com>
2025-09-17 17:08:36 +0500

    Task4

---
+++ Tyuiu.RagozinaAD.Sprint1.Task4.V27.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.RagozinaAD.Sprint1.Task4.V27.Lib;
+namespace Tyuiu.RagozinaAD.Sprint1.Task4.V27.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 2;
+            double y = 1;
+            double wait = 1;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.RagozinaAD.Sprint1.Task4.V27/Program.cs
@@ -0,0 +1,43 @@
+﻿using Tyuiu.RagozinaAD.Sprint1.Task4.V27.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #1 | Выполнила: Рагозина А.Д | АСОиУБ-25-1";
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #1                                                               *");
+        Console.WriteLine("* Тема: Создания итогового решения по спринту                             *");
+        Console.WriteLine("* Задание #4                                                              *");
+        Console.WriteLine("* Вариант #27                                                             *");
+        Console.WriteLine("* Выполнила: Рагозина А.Д | АСОиУБ-25-1                                   *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Условие:                                                                *");
+        Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+        Console.WriteLine("* вычисляет результат по формуле и печатает его на экране.                *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДВННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        int x, y;
+
+        Console.WriteLine("Введите значение x:");
+        x = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите значение y:");
+        y = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("1+sin(Pi*x)/(x-sqrt(|y|)) = "+ds.Calculate(x,y));
+
+        Console.ReadKey();
+
+
+
+    }
+}
