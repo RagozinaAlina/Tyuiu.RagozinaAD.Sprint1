db5017197b5f92e159dbec1fe9984201b1379e36
Алина Рагозина <Tyuiu.RagozinaAD@outlook.com>
2025-09-17 17:09:43 +0500

    Task4

--- Tyuiu.RagozinaAD.Sprint1.Task4.V27.Lib/DataService.cs
+++ Tyuiu.RagozinaAD.Sprint1.Task4.V27.Lib/DataService.cs
@@ -5,7 +5,7 @@ namespace Tyuiu.RagozinaAD.Sprint1.Task4.V27.Lib
     {
         public double Calculate(double x, double y)
         {
-            var res = 1 + Math.Sin(Math.PI * x) / (x - Math.Sqrt(Math.Abs(y)));
+            var res = (1 + Math.Sin(Math.PI * x)) / (x - Math.Sqrt(Math.Abs(y)));
             return Math.Round(res, 3);
         }
             
