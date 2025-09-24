02696115f605e5134b2d1dc78fe932ac083ff67c
Алина Рагозина <Tyuiu.RagozinaAD@outlook.com>
2025-09-24 20:30:47 +0500

    Task7

--- Tyuiu.RagozinaAD.Sprint1.Task7.V18.Lib/DataService.cs
+++ Tyuiu.RagozinaAD.Sprint1.Task7.V18.Lib/DataService.cs
@@ -5,7 +5,7 @@ namespace Tyuiu.RagozinaAD.Sprint1.Task7.V18.Lib
     {
         public double Calculate(double x, double y)
         {
-            double res = ((1 + (Math.Pow(Math.Sin(x+y),2))/ (2 + (Math.Abs(x - (2 * x) / (1 + Math.Pow(x, 2) * Math.Pow(y, 2)))))) + x);
+            double res = ((1 + Math.Pow(Math.Sin(x + y), 2)) / (2 + Math.Abs(x - (2 * x / 1 + Math.Pow(x, 2) * Math.Pow(y, 2))))) + x;
             return Math.Round(res, 3); ;
         }
     }
